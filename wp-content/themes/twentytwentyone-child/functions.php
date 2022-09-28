d60b9ff8a25b07237d660c061b28197550bfe82f
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2022-09-28 14:48:16 -0600

    registering member menu

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -8,17 +8,22 @@ wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', arr
 add_action('wp_enqueue_scripts', 'twentytwentyone_child_styles');
 
 
-add_action( 'widgets_init', 'child_register_sidebar' );
-function child_register_sidebar(){
-	register_sidebar(array(
-		'name' => 'Header',
-		'id' => 'sidebar-2',
-		'before_widget' => '<div id="%2$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h4 class="widgettitle">',
-		'after_title' => '</h4>',
-	));
-}
+// add_action( 'widgets_init', 'child_register_sidebar' );
+// function child_register_sidebar(){
+// 	register_sidebar(array(
+// 		'name' => 'Header',
+// 		'id' => 'sidebar-2',
+// 		'before_widget' => '<div id="%2$s" class="widget %2$s">',
+// 		'after_widget' => '</div>',
+// 		'before_title' => '<h4 class="widgettitle">',
+// 		'after_title' => '</h4>',
+// 	));
+// }
+
+function register_my_menu() {
+	register_nav_menu('member-menu',__( 'Member Menu' ));
+	}
+	add_action( 'init', 'register_my_menu' );
 
 
 
--- wp-content/themes/twentytwentyone-child/template-parts/header/site-nav.php
+++ wp-content/themes/twentytwentyone-child/template-parts/header/site-nav.php
@@ -35,7 +35,31 @@
 	</nav><!-- #site-navigation -->
 <?php endif; ?>
 <?php if ( has_nav_menu( 'footer' ) ) : ?>
-	<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="secondary-navigation">
+	<nav id="site-navigation" class="member-navigation" aria-label="<?php esc_attr_e( 'Member menu', 'twentytwentyone' ); ?>">
+		<div class="menu-button-container">
+			<button id="member-mobile-menu" class="button" aria-controls="member-menu-list" aria-expanded="false">
+				<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
+					<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+				</span>
+				<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'twentytwentyone' ); ?>
+					<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+				</span>
+			</button><!-- #member-mobile-menu -->
+		</div><!-- .menu-button-container -->
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'member-menu',
+				'menu_class'      => 'menu-wrapper',
+				'container_class' => 'member-menu-container',
+				'items_wrap'      => '<ul id="member-menu-list" class="%2$s">%3$s</ul>',
+				'fallback_cb'     => false,
+			)
+		);
+		?>
+
+
+	<!-- <nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="secondary-navigation">
 		<ul class="secondary-mobile-menu">
 			<?php
 			wp_nav_menu(
@@ -51,6 +75,6 @@
 			);
 			?>
 		</ul><!-- .secondary-mobile-menu -->
-	</nav><!-- .secondary-navigation -->
+	<!-- </nav>.secondary-navigation -->
 <?php endif; ?>
 
