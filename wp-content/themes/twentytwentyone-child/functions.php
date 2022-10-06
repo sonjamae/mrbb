f389ca5ab2ca71268b18cd8731e4093a6acd44bf
Shirasa <shirasapraja@gmail.com>
2022-10-06 15:02:59 -0600

    move redirect function to the child theme

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -27,6 +27,17 @@ function add_google_fonts() {
 }
 add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
 
-
-
-?>
+// Redirect function for the SOAER Presentations page.
+add_action('template_redirect', function() {
+    // ID of the redirect page
+    if (!is_page(1730)) {
+        return;
+    }
+    // URL of page with form that needs to be filled before access:
+    if (wp_get_referer() === 'https://sonjamae.ca/mrbb/soaer-committee/soaer-presentations/') {
+        return;
+    }
+	// Redirect to the page with the form:
+    wp_redirect('https://sonjamae.ca/mrbb/soaer-committee/soaer-presentations/');
+    exit;
+} );
